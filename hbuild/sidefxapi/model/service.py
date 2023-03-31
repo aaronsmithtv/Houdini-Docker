d55a19a92bf8f35245cec47bf5da92ad3f87dce2
aaronsmithtv <aaron@aaronsmith.tv>
2023-04-01 00:23:09 +0100

    Add API service and build models

--- hbuild/sidefxapi/model/service.py
+++ hbuild/sidefxapi/model/service.py
@@ -1,8 +1,32 @@
-from pydantic import BaseModel
-
+from pydantic import BaseModel, Field, UrlStr
+from datetime import date
+from enum import Enum
 
 class ApiService(BaseModel):
-	client_id: str
-	client_secret: str
-	access_token_url: str
-	endpoint_url: str
+    client_id: str = Field(..., description="SideFX Application Client ID")
+    client_secret: str = Field(..., description="SideFX Application Client Secret")
+    access_token_url: UrlStr = Field(..., description="The URL for retrieving the access token")
+    endpoint_url: UrlStr = Field(..., description="The base URL for the API endpoint")
+
+class Platform(Enum):
+    WINDOWS = "windows"
+    MACOS = "macos"
+    LINUX = "linux"
+
+class DailyBuild(BaseModel):
+    build: str
+    date: str
+    platform: Platform
+    product: str
+    release: str
+    status: str
+    version: str
+
+class DownloadBuild(BaseModel):
+    date: str
+    download_url: UrlStr
+    filename: str
+    hash: str
+    releases_list: str
+    status: str
+    size: int
